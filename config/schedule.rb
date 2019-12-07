deac53905f3c1f8f33e0d7407a3f413885f4b230
Ubuntu <ubuntu@ip-172-31-33-34.ap-northeast-1.compute.internal>
2019-12-07 08:37:20 +0000

    schedule code 수정

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,11 +5,13 @@ def local(time)
 end
 
 every 10.minutes do
+    rake "jobs:auto_runner_job", :environment => "development"
     rake "jobs:auto_runner_job", :environment => "production"
 end
 
 every 1.minutes do
-    rake "kcm:test", :environment => "production"
+    rake "test:kcm", :environment => "production"
+    rake "test:kcm", :environment => "development"
 end
 ## 스케쥴링 Job 테스트 : 1분 간격으로 Bussiness 모델에 새로운 데이터를 생성합니다.
 # every 1.minutes do
